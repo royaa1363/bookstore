736f717360c777229bd7013f96d26eac36976b73
Roya <parizad.1984@yahoo.com>
2023-08-24 18:48:03 +0330

    update model migrate

--- bookstore/mybook/models.py
+++ bookstore/mybook/models.py
@@ -11,7 +11,7 @@ class Author(models.Model):
 class Book(models.Model):
     book_name = models.CharField(max_length=256, null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateField("date published")
 
     def __str__(self):
         return f" {self.book_name}"
